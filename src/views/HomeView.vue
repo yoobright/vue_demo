90d0a245abeb865fdc95843901c18e7afff2ab7c
yoobright <yxj3179@gmail.com>
2023-03-07 17:42:01 +0800

    add test for wavesurfer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import Vue from "vue";
+import { ref, onMounted } from 'vue'
+import WaveSurfer from 'wavesurfer.js'
+import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.js'
+// import VueWaveSurfer from 'wavesurfer.js-vue'
+
+let wavesurfer: any;
+onMounted(() => {
+  wavesurfer = WaveSurfer.create({
+    container: '#wave',
+    waveColor: 'violet',
+    progressColor: 'purple',
+    plugins: [
+      Timeline.create({
+      container: "#wave-timeline"
+      })
+    ] 
+  });
+  wavesurfer.load('https://wavesurfer-js.org/example/media/demo.wav');
+})
+
+const play_v = () => {
+  wavesurfer.play();
+}
+const pause_v = () => {
+  wavesurfer.pause();
+}
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <h1>测试</h1>
+    <!-- <wavesurfer 
+    ></wavesurfer> -->
+    <el-card class="box-card">
+      <div id="wave"></div>
+      <div id="wave-timeline"></div>
+      <div style="height: 10px;"></div>
+      <ElButton v-on:click="play_v">播放</ElButton>
+      <ElButton v-on:click="pause_v">暂停</ElButton>
+    </el-card>
   </main>
 </template>
+
+<style>
+  .box-card {
+    width: 680px;
+  }
+</style>
