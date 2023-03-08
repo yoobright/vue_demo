9994650db045835353d2572ddbdfc280dab78150
yoobright <yxj3179@gmail.com>
2023-03-08 11:27:34 +0800

    add propgress bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,14 @@ import { ref, onMounted } from 'vue'
 import WaveSurfer from 'wavesurfer.js'
 // @ts-ignore
 import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.js'
-// import VueWaveSurfer from 'wavesurfer.js-vue'
+import audioUrl from '@/assets/canon.mp3'
 
+// import VueWaveSurfer from 'wavesurfer.js-vue'
 let wavesurfer: any;
+const percentage = ref(0)
+const progressDiv = ref();
+const progressBar = ref();
+
 onMounted(() => {
   wavesurfer = WaveSurfer.create({
     container: '#wave',
@@ -16,40 +21,71 @@ onMounted(() => {
     progressColor: 'purple',
     plugins: [
       Timeline.create({
-      container: "#wave-timeline"
+        container: "#wave-timeline"
       })
-    ] 
+    ]
   });
-  wavesurfer.load('https://wavesurfer-js.org/example/media/demo.wav');
+  wavesurfer.load(audioUrl);
+
+
+  const showProgress = function (percent: number) {
+    progressDiv.value.style.display = 'block';
+    percentage.value = percent;
+    // debugger;
+  };
+
+  const hideProgress = function () {
+    progressDiv.value.style.display = 'none';
+  };
+
+  wavesurfer.on('loading', showProgress);
+  wavesurfer.on('ready', hideProgress);
+  wavesurfer.on('destroy', hideProgress);
+  wavesurfer.on('error', hideProgress);
+
 })
 
-const play_v = () => {
-  wavesurfer.play();
-}
-const pause_v = () => {
-  wavesurfer.pause();
-}
 
+const playAudio = () => {
+  if (wavesurfer) {
+    wavesurfer.play();
+  }
+
+}
+const pauseAudio = () => {
+  if (wavesurfer) {
+    wavesurfer.pause();
+  }
+}
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    <h1>测试</h1>
-    <!-- <wavesurfer 
-    ></wavesurfer> -->
     <el-card class="box-card">
-      <div id="wave"></div>
-      <div id="wave-timeline"></div>
+      <div class="waveform" style="position: relative;">
+        <div ref="progressDiv" class="progress-div">
+          <el-progress ref="progressBar" class="progress-bar" :text-inside="true" :stroke-width="20"
+            :percentage="percentage" />
+        </div>
+        <div id="wave"></div>
+        <div id="wave-timeline"></div>
+      </div>
       <div style="height: 10px;"></div>
-      <ElButton v-on:click="play_v">播放</ElButton>
-      <ElButton v-on:click="pause_v">暂停</ElButton>
+      <ElButton @click="playAudio">播放</ElButton>
+      <ElButton @click="pauseAudio">暂停</ElButton>
     </el-card>
   </main>
 </template>
 
 <style>
-  .box-card {
-    width: 680px;
-  }
-</style>
+.box-card {
+  width: 680px;
+}
+
+.progress-div {
+  position: absolute;
+  z-index: 10;
+  top: 50%;
+  left: 5px;
+  right: 5px;
+}</style>
