df23b1d36d1fa3f4cf9089e5e020d4bef228fba3
yoobright <yxj3179@gmail.com>
2023-04-07 16:18:07 +0800

    add heatmap

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -2,10 +2,50 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
+import _ from 'lodash'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
+
+
+function getObjectDiff(obj1: any, obj2: any) {
+  const diff = Object.keys(obj1).reduce((result, key) => {
+      // eslint-disable-next-line no-prototype-builtins
+      if (!obj2.hasOwnProperty(key)) {
+          result.push(key);
+      } else if (_.isEqual(obj1[key], obj2[key])) {
+          const resultKeyIndex = result.indexOf(key);
+          result.splice(resultKeyIndex, 1);
+      }
+      return result;
+  }, Object.keys(obj2));
+
+  return diff;
+}
+
+
+
+describe('test', () => {
+
+  it('test demo', () => {
+    const a = {
+      name: 'test',
+      age: 18,
+      foo: 'bar',
+      id: [2, 3, 4, 6]
+    }
+    const b = {
+      name: 'test2',
+      age: 18,
+      id: [2, 3, 5]
+    }
+    // const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    // expect(wrapper.text()).toContain('Hello Vitest')
+    const out = getObjectDiff(b, a)
+    expect(out).toEqual(['name', 'foo', 'id',])
   })
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,34 @@ import audioUrl from '@/assets/canon.mp3'
 import { colormapJet } from './colormap'
 // @ts-ignore
 // import colormap from 'colormap'
+import Meyda from 'meyda/dist/node/main.js'
+// @ts-ignore
+
+import { use } from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { HeatmapChart } from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  VisualMapComponent,
+  DataZoomComponent,
+} from 'echarts/components';
+import VChart from 'vue-echarts';
+
+use([
+  CanvasRenderer,
+  HeatmapChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  VisualMapComponent,
+  DataZoomComponent,
+]);
+
+
 
 const { t } = useI18n()
 
@@ -29,6 +57,95 @@ const zoomSlider = ref()
 const inputValue = ref()
 const inputFile = ref()
 
+const dialogVisible = ref(false)
+
+function range(start: number, end: number) {
+  return Array.apply(0, Array(end - 1))
+    .map((element, index) => index + start);
+}
+
+const xMax = 420;
+const yMax = 200;
+const xData = range(0, xMax);
+const yData = range(0, yMax);
+
+
+function generateData() {
+  let data = [];
+  for (let i = 0; i <= xMax; i++) {
+    for (let j = 0; j < yMax; j++) {
+      data.push([i, j, Math.random() * 2 - 1]);
+    }
+  }
+  return data;
+}
+const data = generateData();
+const option = ref();
+function initOption() {
+  option.value = {
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: xData
+    },
+    yAxis: {
+      type: 'category',
+      data: yData
+    },
+    dataZoom: [
+        {
+            type: 'slider',
+            show: true,
+            xAxisIndex: [0],
+            startValue: 0,
+            endValue: 512
+        }
+    ],
+    visualMap: {
+      min: -1,
+      max: 1,
+      calculable: false,
+      show: false,
+      // orient: 'horizontal',
+      bottom: 100, 
+      right: 20,
+      realtime: false,
+      inRange: {
+        color: [
+          '#313695',
+          '#4575b4',
+          '#74add1',
+          '#abd9e9',
+          '#e0f3f8',
+          '#ffffbf',
+          '#fee090',
+          '#fdae61',
+          '#f46d43',
+          '#d73027',
+          '#a50026'
+        ]
+      }
+    },
+    series: [
+      {
+        name: 'Gaussian',
+        type: 'heatmap',
+        data: data,
+        emphasis: {
+          // itemStyle: {
+          //   borderColor: '#333',
+          //   borderWidth: 1
+          // }
+          disabled: true
+        },
+        progressive: 2000,
+        animation: true,
+        // silent: true,
+      }
+    ]
+  }
+}
+
 // const colors = colormap({
 //   colormap: 'jet',
 //   nshades: 256,
@@ -42,7 +159,7 @@ onMounted(() => {
     waveColor: '#95d475',
     progressColor: '#529b2e',
     scrollParent: true,
-    // backend: 'MediaElement',
+    backend: 'MediaElementWebAudio',
     plugins: [
       Timeline.create({
         container: '#wave-timeline',
@@ -73,11 +190,124 @@ onMounted(() => {
     progressDiv.value.style.display = 'none'
   }
 
+  const seekFunction = function (percent: number) {
+    console.log(percent)
+  }
+
+  const readyFunction = function () {
+    console.log("ready")
+    progressDiv.value.style.display = 'none'
+    initOption();
+  }
+
+
   wavesurfer.on('loading', showProgress)
   wavesurfer.on('ready', hideProgress)
   wavesurfer.on('destroy', hideProgress)
   wavesurfer.on('error', hideProgress)
+  wavesurfer.on('seek', seekFunction)
+  wavesurfer.on('waveform-ready', readyFunction)
+
+  // console.log(document.getElementById('heatmap'))
+  // console.log("heatmap")
+
 })
+
+function getInputBuffer(length: number = 256, sampleSize: number = 512) {
+  const bufferLength =  wavesurfer.backend.buffer.length;
+  const neededLength = length * sampleSize;
+  const numberOfChannels = wavesurfer.backend.buffer.numberOfChannels;
+  if (bufferLength < neededLength) {
+    throw new Error('Buffer is too short for specified parameters');
+  }
+  const startPercents = wavesurfer.backend.getPlayedPercents();
+  const startIndx = Math.round(startPercents * bufferLength);
+  const endIndx = startIndx + neededLength;
+  let inputStartIndex;
+  let inputEndIndex;
+  if (endIndx > bufferLength) {
+    inputStartIndex = bufferLength - neededLength;
+    inputEndIndex = bufferLength;
+  } else {
+    inputStartIndex = startIndx;
+    inputEndIndex = endIndx;
+  }
+
+  const output = [];
+  for (let n = 0; n < numberOfChannels; n++) {
+    const channelData = wavesurfer.backend.buffer.getChannelData(n);
+    output.push(channelData.slice(inputStartIndex, inputEndIndex));
+  }
+
+  return output;
+}
+
+function getAllfeature(featureName: string, length: number = 256, sampleSize: number = 512) {
+  const bufferData = getInputBuffer(length, sampleSize);
+  const numberOfChannels = bufferData.length;
+  const res = [];
+  for (let n = 0; n < numberOfChannels; n++) {
+    const channelData = bufferData[n];
+    for (let i = 0; i < channelData.length; i += Meyda.bufferSize) {
+      const buffer = channelData.slice(i, i + Meyda.bufferSize);
+      if (buffer.length < Meyda.bufferSize) {
+        break;
+      }
+      res.push(Meyda.extract(featureName, buffer));
+    }
+  }
+  return res;
+}
+
+
+const testFunction = () => {
+  if (wavesurfer) {
+    // console.log(wavesurfer.backend);
+    const featureName = "amplitudeSpectrum";
+    const numberOfChannels = wavesurfer.backend.buffer.numberOfChannels;
+    const sampleSize = 512;
+    const maxLen = 256;
+
+    Meyda.bufferSize = sampleSize;
+    Meyda.sampleRate = wavesurfer.backend.ac.sampleRate;
+    const res = getAllfeature(featureName, maxLen, sampleSize);
+
+    
+
+    // console.log(res);
+    // const xMax: number = res.length;
+    const xMax: number = Math.min(res.length, maxLen);
+    const yMax: number = Math.min(res[0].length, 256);
+    const kMax: number = res.length / numberOfChannels;
+    console.log(xMax, yMax, kMax);
+    option.value.xAxis.data = range(0, xMax);
+    option.value.yAxis.data = range(0, yMax);
+    const data = [];
+    let max = -Infinity;
+    let min = Infinity;
+    for (let i = 0; i < xMax; i++) {
+      for (let j = 0; j < yMax; j++) {
+        let d = 0;
+        for (let k = 0; k < numberOfChannels; k++) {
+          d += res[i + k * kMax][j];
+        }
+        d = Math.max(-255, Math.log10(d) * 45);
+        if (d > max) {
+          max = d;
+        }
+        if (d < min) {
+          min = d;
+        }
+        data.push([i, j, d]);
+      }
+    }
+    option.value.series[0].data = data;
+    option.value.visualMap.min = min;
+    option.value.visualMap.max = max;
+    option.value.series[0].name = featureName;
+    // dialogVisible.value = true
+  }
+}
 
 const playPauseAudio = () => {
   if (wavesurfer) {
@@ -106,9 +336,11 @@ const readFile = () => {
     if (wavesurfer.isReady) {
       wavesurfer.loadBlob(file)
     }
-    
+
   }
 }
+
+
 </script>
 
 <template>
@@ -117,25 +349,14 @@ const readFile = () => {
       <h3>{{ t('test') }}</h3>
     </div>
     <div class="footer-div">
-      <el-input
-        style="width: 680px"
-        type="file"
-        ref="inputFile"
-        @change="readFile()"
-        v-model="inputValue"
-      />
+      <el-input style="width: 680px" type="file" ref="inputFile" @change="readFile()" v-model="inputValue" />
     </div>
     <div class="main-div">
       <el-card class="box-card">
         <div class="waveform" style="position: relative">
           <div ref="progressDiv" class="progress-div">
-            <el-progress
-              ref="progressBar"
-              class="progress-bar"
-              :text-inside="true"
-              :stroke-width="20"
-              :percentage="percentage"
-            />
+            <el-progress ref="progressBar" class="progress-bar" :text-inside="true" :stroke-width="20"
+              :percentage="percentage" />
           </div>
           <div id="wave"></div>
           <div id="wave-timeline"></div>
@@ -144,32 +365,40 @@ const readFile = () => {
         <div style="height: 10px"></div>
         <div class="footer-div">
           <ElButton @click="playPauseAudio" style="width: 100px">{{ t('play_puase') }}</ElButton>
+          <ElButton @click="testFunction" style="width: 100px">{{ t('test') }}</ElButton>
           <el-icon style="margin-left: 12px" :size="20">
             <i-ep-zoom-out />
           </el-icon>
-          <el-slider
-            @input="changeZoom"
-            :min="1"
-            :max="100"
-            style="width: 100px; margin-left: 12px; margin-right: 12px"
-            v-model="zoomValue"
-          />
+          <el-slider @input="changeZoom" :min="1" :max="100" style="width: 100px; margin-left: 12px; margin-right: 12px"
+            v-model="zoomValue" />
           <el-icon :size="20" style="margin-right: 12px">
             <i-ep-zoom-in />
           </el-icon>
           <el-icon :size="20">
             <i-ep-microphone />
           </el-icon>
-          <el-slider
-            @input="changeVolume"
-            :min="0"
-            :max="100"
-            style="width: 100px; margin-left: 12px; margin-right: 12px"
-            v-model="volumeValue"
-          />
+          <el-slider @input="changeVolume" :min="0" :max="100" style="width: 100px; margin-left: 12px; margin-right: 12px"
+            v-model="volumeValue" />
         </div>
       </el-card>
+      <el-card class="box-card" style="height: 504px;">
+        <div class="main-div">
+          <v-chart class="heatmap" :option="option" autoresize />
+        </div>
+      </el-card>
+
     </div>
+    <el-dialog v-model="dialogVisible" title="Tips" width="30%">
+
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </el-main>
 </template>
 
@@ -198,5 +427,14 @@ const readFile = () => {
   justify-content: center;
   align-items: center;
   height: 50px;
+}
+
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+
+.heatmap {
+  height: 400px;
+  width: 600px;
 }
 </style>
